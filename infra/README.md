13edb7783e3691d0632301a5fc41ba91044cc409
Flavio Augusto Rodrigues Tavares <flaviotvrs@gmail.com>
2022-05-06 18:29:19 -0300

    adding two more nodes

--- infra/README.md
+++ infra/README.md
@@ -1,3 +1,13 @@
 # MongoDB Infrastructure
 
 This code provisions a basic Linux Virtual Machine, with Ubuntu 20.04, with a Public IP and a Network Security Group
+
+## Installing MongoDB
+
+```
+wget https://repo.mongodb.org/apt/ubuntu/dists/bionic/mongodb-org/5.0/multiverse/binary-amd64/mongodb-org-server_5.0.8_amd64.deb
+
+sudo dpkg -i mongodb-org-server_5.0.8_amd64.deb
+
+mongod --version
+```
--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,11 @@ resource "azurerm_resource_group" "main" {
   tags = {
     ResponsibleTeam = "Database & Storage"
   }
+  lifecycle {
+    ignore_changes = [
+      tags["Creator"]
+    ]
+  }
 }
 
 module "mongodb-node-01" {
@@ -11,4 +16,18 @@ module "mongodb-node-01" {
     resource_group_name = azurerm_resource_group.main.name
     server_name = "mongodb-node-01"
     location = "eastus2"
+}
+
+module "mongodb-node-02" {
+    source = "./ubuntu-server"
+    resource_group_name = azurerm_resource_group.main.name
+    server_name = "mongodb-node-02"
+    location = "eastus2"
+}
+
+module "mongodb-node-03" {
+    source = "./ubuntu-server"
+    resource_group_name = azurerm_resource_group.main.name
+    server_name = "mongodb-node-03"
+    location = "centralus"
 }
