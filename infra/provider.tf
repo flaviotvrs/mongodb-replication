8d2ad3a20de88ff04aa576d1e9ac816d469de706
Flavio Augusto Rodrigues Tavares <flaviotvrs@gmail.com>
2022-05-06 18:24:48 -0300

    configuring provider

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = "1.1.7"
+
+  backend "local" {}
+
+  required_providers {
+    azurerm = {
+      # source  = "hashicorp/azurerm"
+      version = "=3.5.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
