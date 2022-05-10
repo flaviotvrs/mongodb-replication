6cd8b42541159958c00d4aa642a980fc2a4437c9
Flavio Augusto Rodrigues Tavares <flaviotvrs@gmail.com>
2022-05-10 15:51:39 -0300

    adding one more network in centralus with another node

--- infra/main.tf
+++ infra/main.tf
@@ -11,17 +11,31 @@ resource "azurerm_resource_group" "main" {
   }
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "replset-network"
+resource "azurerm_virtual_network" "eastus2" {
+  name                = "eastus2-replset-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
+  location            = "eastus2"
   resource_group_name = azurerm_resource_group.main.name
 }
 
-resource "azurerm_subnet" "main" {
+resource "azurerm_subnet" "eastus2" {
   name                 = "mongodb"
   resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  virtual_network_name = azurerm_virtual_network.eastus2.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_virtual_network" "centralus" {
+  name                = "centralus-replset-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "centralus"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "centralus" {
+  name                 = "mongodb"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.centralus.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
@@ -29,7 +43,7 @@ module "mongodb-node-01" {
     source = "./ubuntu-server"
     resource_group_name = azurerm_resource_group.main.name
     private_ip_address = "10.0.2.4"
-    subnet_id = azurerm_subnet.main.id
+    subnet_id = azurerm_subnet.eastus2.id
     server_name = "node-01"
     location = "eastus2"
 }
@@ -38,16 +52,16 @@ module "mongodb-node-02" {
     source = "./ubuntu-server"
     resource_group_name = azurerm_resource_group.main.name
     private_ip_address = "10.0.2.5"
-    subnet_id = azurerm_subnet.main.id
+    subnet_id = azurerm_subnet.eastus2.id
     server_name = "node-02"
     location = "eastus2"
 }
 
-# module "mongodb-node-03" {
-#     source = "./ubuntu-server"
-#     resource_group_name = azurerm_resource_group.main.name
-#     private_ip_address = "10.0.2.6"
-#     subnet_id = azurerm_subnet.main.id
-#     server_name = "node-03"
-#     location = "centralus"
-# }
+module "mongodb-node-03" {
+    source = "./ubuntu-server"
+    resource_group_name = azurerm_resource_group.main.name
+    private_ip_address = "10.0.2.6"
+    subnet_id = azurerm_subnet.centralus.id
+    server_name = "node-03"
+    location = "centralus"
+}
